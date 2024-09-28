a19f32bfd135601bb34722cf53dd5b2145bb7baa
Yuriy Emelin <yuem-1@yandex.ru>
2024-09-28 21:04:00 +0300

    add admin

--- recipeSite/recipeSiteapp/urls.py
+++ recipeSite/recipeSiteapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.static import static
 # from django.conf.urls import handler404, handler500
 from django.urls import path
 
-from . import views
+from ..recipeSiteapp import views
 from ..recipeSite import settings
 
 urlpatterns = [
